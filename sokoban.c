faca903920ffd6a7690a1e1f9a225015533a15ab
PPPosik <eoanswkgksk@gmail.com>
2017-06-05 17:36:03 +0900

    이름 입력

--- sokoban.c
+++ sokoban.c
@@ -2,6 +2,7 @@
 #include <termio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <unistd.h>
 
 #define Y 30
 #define X 30
@@ -15,13 +16,17 @@ int x, y;
 int stage = -1;
 int box_cnt[STAGE];
 int clear_cnt[STAGE];
+char username[11];
 
 int getch();
 void MapLoad();
 void MapDraw();
 void MapClear();
 void PlayerMove();
+void UserName();
+void FileLoad();
 bool StageClear();
+void Option(char key);
 
 int getch(void){
 
@@ -128,6 +133,10 @@ void MapLoad()
 
 void MapDraw()
 {
+  StageClear();
+
+  printf("    Hello %s\n\n", username);
+
   for(int i = 0; i < Y; i++)
   {
     for(int j = 0; j < X; j++)
@@ -136,7 +145,6 @@ void MapDraw()
     }
     printf("\n");
   }
-  StageClear();
 }
 
 void MapClear()
@@ -145,10 +153,146 @@ void MapClear()
   system("clear");
 }
 
+void Save()
+{
+  FILE *save;
+  int i;
+
+  save = fopen("sokoban.txt", "w");
+
+  fprintf(save, "%s\n", username);
+
+  fprintf(save, "%d\n", stage);
+
+  i = stage;
+
+  for(int j = 0; j < Y; j++)
+  {
+    for(int k = 0; k < X; k++)
+    {
+      fprintf(save, "%c", map[i][j][k]);
+    }
+    fprintf(save, "\n");
+  }
+
+  fclose(save);
+}
+
+void FileLoad()
+{
+  FILE *fileload;
+  char ch;
+  int i = 0;
+  int j;
+  int load_map[Y][X];
+  int load_x = 0, load_y = -2;
+  int line_cnt = 0;
+
+  for(int i = 0; i < 11; i++)
+  {
+    username[i] = ' ';
+  }
+
+  fileload = fopen("sokoban.txt", "r");
+
+  while(fscanf(fileload, "%c", &ch) != EOF)
+  {
+    if(ch == '\n')
+    {
+      load_x = 0;
+      load_y++;
+      line_cnt++;
+    }
+    else if ((line_cnt == 0) && (('a' <= ch && ch <= 'z') || ('A' <= ch && ch <= 'Z')))
+    {
+      username[i] = ch;
+      ++i;
+    }
+    else if ((line_cnt == 1) && ('0' <= ch && ch <= '9'))
+    {
+      stage = ch - '0';
+    }
+    else
+    {
+      load_map[load_y][load_x] = ch;
+      load_x++;
+    }
+
+    if(ch == '@')
+    {
+      player_y[stage] = load_y;
+      player_x[stage] = load_x - 1;
+    }
+  }
+
+  username[++i] = '\0';
+
+  for(int a = 0; a < Y; a++)
+  {
+    for(int b = 0; b < X; b++)
+    {
+      map[stage][a][b] = load_map[a][b];
+    }
+  }
+
+  fclose(fileload);
+}
+
+void UserName()
+{
+  bool flag;
+  int i = 0;
+  int length;
+
+  printf("Start....\n");
+
+  for(int i = 0; i < 11; i++)
+  {
+    username[i] = ' ';
+  }
+
+  while(1)
+  {
+    flag = true;
+    length = 0;
+    printf("input name : ");
+    scanf("%s", username);
+
+    for(int i = 0; i < 11; i++)
+    {
+      if(!(('a' <= username[i] && username[i] <= 'z') || ('A' <= username[i] && username[i] <= 'Z')) && (username[i] != '\0'))
+      {
+        flag = false;
+      }
+
+      if(username[i] == '\0')
+        break;
+    }
+
+    while(username[length] != '\0')
+    {
+      if(length > 9)
+        flag = false;
+
+      length++;
+    }
+
+    if(flag)
+      break;
+
+    printf("name must be in english or do not exceed 10 letters.\n");
+  }
+
+  sleep(1);
+  MapClear();
+}
+
 void PlayerMove()
 {
   char key;
   int dx = 0, dy = 0;
+
+  printf("(Command)");
 
   key = getch();
 
@@ -174,16 +318,14 @@ void PlayerMove()
       dx = 1;
       break;
 
-    case 'e':
-    case 'E':
-      printf("프로그램을 종료합니다.");
-      exit(0);
-      break;
-
     case '*':
       stage++;
       break;
   }
+
+  if(((((((key != 'h' || key != 'j') || key != 'k') || key != 'l') || key != 'H') || key != 'J') || key != 'K') || key != 'L')
+    Option(key);
+
 
   if(map[stage][player_y[stage] + dy][player_x[stage] + dx] == '#')
   {
@@ -249,8 +391,38 @@ bool StageClear()
   return flag;
 }
 
+void Option(char key)
+{
+  char enter;
+
+  printf(" %c", key);
+  enter = getch();
+  if(enter == '\n')
+  {
+    switch (key) {
+        case 's':
+        case 'S':
+          Save();
+          break;
+
+        case 'f':
+        case 'F':
+          FileLoad();
+          break;
+
+        case 'e':
+        case 'E':
+          printf("SEE YOU %s . . . .", username);
+          Save();
+          exit(0);
+          break;
+    }
+  }
+}
+
 int main()
 {
+  UserName();
   MapLoad();
 
   while(1)
